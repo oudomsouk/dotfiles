b5427bf7f8eba2739f599aa2e835154b409a49f7
dirron <dillonoudomsouk@gmail.com>
2017-10-30 17:50:23 -0400

    Update battery.sh

--- Scripts/battery.sh
+++ Scripts/battery.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
-#output=echo upower -d | grep "time to empty" | head -1
-#notify-send -u critical -t 3000 "$output"
-
-status=`acpi`
-notify-send "$status"
+while true; do
+  output=`acpi`
+  value=`echo $output | egrep -o [0-9][0-9]% | egrep -o [0-9][0-9]`
+  status=`echo $output | egrep -o Discharging`
+  if [[ $value -le "20" && $status == "Discharging" ]]
+  then
+    notify-send -u critical -t 5000 "Battery is low!" \
+      "${output}%"
+  fi
+  sleep 60
+done
